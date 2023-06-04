2bbecad1653804345bc3bf3fc12fd64852d05954
Euler Magno <eulermagno.mstrg@gmail.com>
2023-06-04 13:06:01 -0300

    Create ex018_training_while.py
    File with while fixing exercise

---
+++ ex018_training_while.py
@@ -0,0 +1,13 @@
+
+nome = input('Digite seu nome: ')
+
+indice = 0
+novo_nome = ''
+
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+print(f'{novo_nome}*')
+
